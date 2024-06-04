7399c163703f27e7a11b1515c2f80c121c6cb6e0
OfirSarfaty <ofir.sa@bluewhite.co>
2024-06-04 10:35:34 +0300

    Refactor app initialization to previous solution

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://weatheruser:asdfg@localhost/weatherdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('WEATHER_SERVICE_DATABASE_URL', 'postgresql://weatheruser:asdfg@localhost/weatherdb')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-    db.init_app(app)
-
-    with app.app_context():
-        # Import routes inside the app context to avoid circular imports
-        from . import routes
-
-    return app
+from app import routes
